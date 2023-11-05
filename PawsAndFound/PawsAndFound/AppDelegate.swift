853da46e837d159b0dfc1f34f65cd99b10f6d3e7
JoseBaez010 <josebaez0968@gmail.com>
2023-11-05 11:20:39 -0500

    Added the base code

---
+++ PawsAndFound/PawsAndFound/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  PawsAndFound
+//
+//  Created by Jose Baez on 10/30/23.
+//
+
+import UIKit
+import ParseSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        ParseSwift.initialize(applicationId: "9mnVuaIWtTVH83CU5rED4zappVxgFPrZHuZbutzw",
+                              clientKey: "zEyBdMk1e5f5XxKs50745OYdqneKeZ2voTCLIeQt",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
