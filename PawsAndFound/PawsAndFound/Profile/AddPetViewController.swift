9f113f34d16a4cdd50d39022ef234b8276b31d6a
JoseBaez010 <josebaez0968@gmail.com>
2023-11-11 21:44:28 -0500

    Added another page to profile

---
+++ PawsAndFound/PawsAndFound/Profile/AddPetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPetViewController.swift
+//  PawsAndFound
+//
+//  Created by Jose Baez on 11/11/23.
+//
+
+import UIKit
+
+class AddPetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PawsAndFound/PawsAndFound/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  PawsAndFound
+//
+//  Created by Jose Baez on 11/11/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    
+    // TODO: Add the Profile Stuff here
+}
