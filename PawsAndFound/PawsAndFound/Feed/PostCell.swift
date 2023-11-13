ac42ae724bfd9dbc6e04b36f7d7f97081f830052
Allan Prieb <81725183+allanprieb@users.noreply.github.com>
2023-11-13 12:18:55 -0500

    Update feed
    - [x] Removed blurred effect on images
    - [x] Removed 10 post limit
    - [x] Removed 24 hour post age limit

--- PawsAndFound/PawsAndFound/Feed/PostCell.swift
+++ PawsAndFound/PawsAndFound/Feed/PostCell.swift
@@ -17,7 +17,6 @@ class PostCell: UITableViewCell {
     @IBOutlet private weak var dateLabel: UILabel!
 
     // Blur view to blur out "hidden" posts
-    @IBOutlet private weak var blurView: UIVisualEffectView!
 
     private var imageDataRequest: DataRequest?
 
@@ -54,27 +53,27 @@ class PostCell: UITableViewCell {
             dateLabel.text = DateFormatter.postFormatter.string(from: date)
         }
 
-        // TODO: Pt 2 - Show/hide blur view
-        // A lot of the following returns optional values so we'll unwrap them all together in one big `if let`
-        // Get the current user.
-        if let currentUser = User.current,
-
-            // Get the date the user last shared a post (cast to Date).
-           let lastPostedDate = currentUser.lastPostedDate,
-
-            // Get the date the given post was created.
-           let postCreatedDate = post.createdAt,
-
-            // Get the difference in hours between when the given post was created and the current user last posted.
-           let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
-
-            // Hide the blur view if the given post was created within 24 hours of the current user's last post. (before or after)
-            blurView.isHidden = abs(diffHours) < 24
-        } else {
-
-            // Default to blur if we can't get or compute the date's above for some reason.
-            blurView.isHidden = false
-        }
+//        // TODO: Pt 2 - Show/hide blur view
+//        // A lot of the following returns optional values so we'll unwrap them all together in one big `if let`
+//        // Get the current user.
+//        if let currentUser = User.current,
+//
+//            // Get the date the user last shared a post (cast to Date).
+//           let lastPostedDate = currentUser.lastPostedDate,
+//
+//            // Get the date the given post was created.
+//           let postCreatedDate = post.createdAt,
+//
+//            // Get the difference in hours between when the given post was created and the current user last posted.
+//           let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
+//
+//            // Hide the blur view if the given post was created within 24 hours of the current user's last post. (before or after)
+//            blurView.isHidden = abs(diffHours) < 24
+//        } else {
+//
+//            // Default to blur if we can't get or compute the date's above for some reason.
+//            blurView.isHidden = false
+//        }
     }
 
     override func prepareForReuse() {
